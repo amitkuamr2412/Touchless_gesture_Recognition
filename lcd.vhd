5b2bacbcab5aa69f4151a6d28a25ddc883e0d9e8
Amit Kumar <32342792+Ultron2412@users.noreply.github.com>
2018-05-11 04:17:32 +0530

    updated

--- lcd.vhd
+++ lcd.vhd
@@ -1,28 +1,16 @@
+------------------# To control the LCD module display #-------------------
 library IEEE;
-
 use IEEE.STD_LOGIC_1164.ALL;
-
 use IEEE.STD_LOGIC_ARITH.ALL;
-
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
- 
-
 entity lcd is
-
 port ( clk ,lt,rt,up,down : in std_logic;                          --clock i/p
-
        lcd_rw : out std_logic;                         --read & write control
-
        lcd_e : out std_logic;                         --enable control
-
        lcd_rs : out std_logic;                         --data or command control
-
        data  : out std_logic_vector(7 downto 0));     --data line
-
 end lcd;
-
- 
 
 architecture Behavioral of lcd is
 
@@ -41,72 +29,55 @@ begin
 lcd_rw <= '0';  --lcd write
 
 process(clk)
-
 variable i : integer := 0;
-
 variable j : integer := 1;
 
 begin
-
- 
-
 if clk'event and clk = '1' then
 
 if i <= 1000000 then
-
 i := i + 1;
-
 lcd_e <= '1';
 
 if (lt = '0' and rt = '0' and up = '0' and down = '0') then  
 data <= dataN(j)(7 downto 0);
 end if;
+
 if lt = '1' then
 data <= dataL(j)(7 downto 0);
 end if ;
+ 
 if rt = '1' then
 data <= dataR(j)(7 downto 0);
 end if; 
+
 if up = '1' then
 data <= dataU(j)(7 downto 0);
 end if ;
+ 
 if down = '1' then
 data <= dataD(j)(7 downto 0);
 end if ;
 
 elsif i > 1000000 and i < 2000000 then
-
 i := i + 1;
-
 lcd_e <= '0';
-
+ 
 elsif i = 2000000 then
-
 j := j + 1;
-
 i := 0;
-
 end if;
 
 if j <= 5  then
-
 lcd_rs <= '0';    --command signal
-
 elsif j > 5   then
-
 lcd_rs <= '1';   --data signal
-
 end if;
 
 if j = 11 then  --repeated display of data
-
 j := 5;
-
 end if;
-
 end if;
-
- 
 
 end process;
 
